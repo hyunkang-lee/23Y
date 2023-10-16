bec4d38c746ff324da2fefa7cc4ab48d2fc2cbf1
hyunkang-lee <klun102w@gmail.com>
2023-10-16 18:46:51 +0900

    다하자

--- chap12-collection-practice-source/src/main/java/com/ohgiraffers/hw1/controller/BookManager.java
+++ chap12-collection-practice-source/src/main/java/com/ohgiraffers/hw1/controller/BookManager.java
@@ -1,45 +1,61 @@
 package com.ohgiraffers.hw1.controller;
 
+import com.ohgiraffers.hw1.comparator.AscBookNo;
+import com.ohgiraffers.hw1.comparator.AscBookTitle;
+import com.ohgiraffers.hw1.comparator.DescBookNo;
+import com.ohgiraffers.hw1.comparator.DescBookTitle;
 import com.ohgiraffers.hw1.model.dto.BookDTO;
+import com.ohgiraffers.hw1.view.BookMenu;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BookManager {
+    BookMenu bmn = new BookMenu();
+    private ArrayList<BookDTO> bookList = new ArrayList<>();
     private Scanner sc = new Scanner(System.in);
-    private ArrayList<BookDTO> bookList(){
-
-        return bookList();
-    }
 
     public BookManager() {
     }
-    public void addBook(){
-        // Book book?
-    }
-    public int deleteBook (){
-        int index = 0;
-        return index;
-    }
-    public String searchBook (){
-        String btitle = "";
 
-        return btitle;
-    }
-    public void displayAll(){
-
-    }
-    public List<BookDTO> sortedBookList(int type){
-
-
-        return sortedBookList(1);
-    }
-    public void printBookList(List<BookDTO>printlist){
+    public void addBook(BookDTO booklist) {
 
 
     }
 
+    public int deleteBook() {
+    }
+
+    public String searchBook() {
+
+    }
+
+    public void displayAll() {
 
 
+        }
+
+    }
+
+    public List<BookDTO> sortedBookList(int type) {
+        AscBookTitle asc = new AscBookTitle();
+
+        switch (type){
+            case 1 : bookList.sort(new AscBookNo()); break;
+            case 2 : bookList.sort(new DescBookNo()); break;
+            case 3 : bookList.sort(new AscBookTitle()); break;
+            case 4 : bookList.sort(new DescBookTitle()); break;
+
+        }
+        return bookList;
+
+    }
+
+    public void printBookList(List<BookDTO> printlist) {
+
+        }
+    }
 }
+
+
